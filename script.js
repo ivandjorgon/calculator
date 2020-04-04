1c75af4c50fa258a8fdfd84c82492f54f15c539b
Ivan Djorgon <ivan_djorgon@hotmail.com>
2020-04-03 19:39:16 -0500

    Add math functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function toAdd(a, b) {
+    return a + b;
+}
+
+function toSubtract(a, b) {
+    return a - b;
+}
+
+function toMultiply(a, b) {
+    return a * b;
+}
+
+function toDivide(a, b) {
+    return a / b;
+}
+
+toAdd(10, 2); // Will return 12
+toSubtract(10, 2); // Will return 8
+toMultiply(10, 2); // Will return 20
+toDivide(10, 2); // Will return 5
