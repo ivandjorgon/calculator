c4d6c94a881c980435fe2a7559c5f2fc237077d3
Ivan Djorgon <ivan_djorgon@hotmail.com>
2020-04-15 23:51:21 -0500

    Update comments

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let x = "";
 let y = "";
 let operator = "";
 let results = "";
-let gotResults = false; //mitigates errors
+let gotResults = false;
 
 const p = document.querySelector("#results-display");
 p.textContent = "0.00";
@@ -35,7 +35,6 @@ function clear() {
   p.textContent = "0.00";
 }
 
-//gives number buttons 0-9 functionality
 const numbers = document.querySelectorAll(".number");
 numbers.forEach((numberButton) => {
   numberButton.addEventListener("click", (e) => {
@@ -48,14 +47,12 @@ numbers.forEach((numberButton) => {
   });
 })
 
-//gives operator buttons -+/* functionality
 const operators = document.querySelectorAll(".operator");
 operators.forEach((operatorButton) => {
   operatorButton.addEventListener("click", (e) => {
     gotResults = false;
     switch(true){
 
-      //populates x and operator and transfers info from h1 to p
       case (p.textContent == "0.00" && h1.textContent != "" && h1.textContent !== "-"):
       x = h1.textContent;
       operator = e.target.textContent;
@@ -63,17 +60,14 @@ operators.forEach((operatorButton) => {
       h1.textContent = "";
       break;
 
-      //puts a negative symbol into h1, making x negative
       case (p.textContent == "0.00" && e.target.textContent == "-" && h1.textContent == ""):
       h1.textContent = "-";
       break;
 
-      //makes y negative
       case ( x != "" && operator != "-" && e.target.textContent == "-" && h1.textContent == ""):
       h1.textContent = "-";
       break;
 
-      //makes a new operation using the number given from a previous operation
       case (h1.textContent != "" && p.textContent != "" && h1.textContent != "-" && gotResults):
       x = h1.textContent;
       operator = e.target.textContent;
@@ -81,13 +75,11 @@ operators.forEach((operatorButton) => {
       h1.textContent = "";
       break;
 
-      //changes operator in p
       case (operator != e.target.textContent && h1.textContent != "-" && p.textContent != "0.00" && h1.textContent == ""):
       operator = e.target.textContent;
       p.textContent = p.textContent.slice(0,-1) + operator;
       break;
 
-      //makes operator buttons perform evaluate() if pressed while x exists
       case (x != "" && operator != "" && h1.textContent != "" && !p.textContent.includes("=") && h1.textContent != "-"):
       y = h1.textContent;
       h1.textContent = "";
@@ -97,7 +89,6 @@ operators.forEach((operatorButton) => {
       operator = e.target.textContent;
       break;
 
-      //fixes error when trying to add operations after hitting equals
       case p.textContent.includes("="):
       x = h1.textContent;
       operator = e.target.textContent;
@@ -120,7 +111,7 @@ equal.addEventListener("click", (e) => {
     h1.textContent = evaluate(Number(x),operator,Number(y));
     results  = evaluate(Number(x),operator,Number(y));
   } else if (p.textContent.includes("=")){
-    //ignores equal button if equal symbol is in p
+
   }
 });
 
